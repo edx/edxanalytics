112398fd8634ddef85481bd68733e2efe0bfc47d
Piotr Mitros <pmitros@mitx.mit.edu>
2013-03-29 16:06:25 -0400

    Remove absolute path reference; moved dashboard

--- anserv/dashboard/views.py
+++ anserv/dashboard/views.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 
 def dashboard(request):
     if request.user.is_authenticated():
-        return render(request, 'analytics-experiments/anserv/dashboard/static/dashboard.html')
+        return render(request, 'dashboard/dashboard.html')
     else:
        return HttpResponseRedirect(reverse("django.contrib.auth.views.login"))
 
