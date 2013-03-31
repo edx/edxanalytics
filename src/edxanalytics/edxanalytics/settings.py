a1b91b447583ea92bfb1c382625719b1da205dac
Piotr Mitros <pmitros@mitx.mit.edu>
2013-03-31 10:43:50 -0400

    Momentarily disabled pipeline

--- src/edxanalytics/edxanalytics/settings.py
+++ src/edxanalytics/edxanalytics/settings.py
@@ -33,7 +33,6 @@ SNS_SUBSCRIPTIONS = []
 
 #### Remaining settings
 
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -150,6 +149,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "templates"
 )
 
 INSTALLED_APPS = (
