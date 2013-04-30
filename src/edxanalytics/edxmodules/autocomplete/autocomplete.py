87970b84ad73a772a19754bcd7d9f73c33971c46
Piotr Mitros <pmitros@mitx.mit.edu>
2013-04-30 19:29:14 -0400

    Missed commit on autocomplete

---
+++ src/edxanalytics/edxmodules/autocomplete/autocomplete.py
@@ -0,0 +1,11 @@
+from djanalytics.core.decorators import query
+
+@query()
+def autocomplete_available():
+    ''' Returns a list of parameters for which the system has an
+    autocomplete. '''
+    return ['user']
+
+@query()
+def autocomplete(parameter, start):
+    return ['pmitros', 'piotr', 'peter']
