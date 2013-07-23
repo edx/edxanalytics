5637be31781ec6727f930ce3731da79639b41927
Juho Kim <juhokim@edx.org>
2013-07-23 19:19:40 +0000

    correct url for runserver

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ LMS_DIR=/opt/edx/edx-platform
 LMS_PORT=8000
 
 cd $EDXANALYTICS_DIR/src/edxanalytics
-python manage.py runserver 127.0.0.1:$EDXANALYTICS_PORT &
+python manage.py runserver 0.0.0.0:$EDXANALYTICS_PORT &
 sleep 1
 
 cd $LMS_DIR
