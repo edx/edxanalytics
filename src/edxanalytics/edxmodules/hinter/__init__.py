7c21145fa50c929864fd67609000b27f589941e5
Felix Sun <fephsun@gmail.com>
2013-07-25 15:14:19 -0400

    Finished migrating instructor view to insights.

--- src/edxanalytics/edxmodules/hinter/__init__.py
+++ src/edxanalytics/edxmodules/hinter/__init__.py
@@ -6,7 +6,7 @@ could be expanded to use other data.
 Databases:
 userdata - tracks user actions on problems.  Each document:
 - 'user'
-- 'problem' - a location
+- 'location' - a location
 - 'hints_shown' - a list of ids of hints shown
 - 'previous_answers' - a list of previous answers
 - 'voted' - whether the user has voted already
@@ -18,7 +18,7 @@ settings - problem-level settings.  Each document:
 - 'debug'
 
 hints - has all of the hints.  Each document:
-- 'problem'
+- 'location'
 - 'user' - the user who submitted this hint
 - 'answer' - the wrong answer that this hint is for
 - 'hint' - the text of the hint
@@ -75,7 +75,7 @@ def hinting_setup(mongodb, events):
         userdata = mongodb['userdata']
         spec = {
             'user': evt['user'],
-            'problem': location,
+            'location': location,
         }
         user = userdata.find_one(spec)
         if user is None:
@@ -116,10 +116,10 @@ def get_hint(mongodb, query, in_dict_json):
     # answer.
     hints = mongodb['hints']
     if moderate:
-        all_hints = hints.find({'problem': location,
+        all_hints = hints.find({'location': location,
                                 'settings.approved': True})
     else:
-        all_hints = hints.find({'problem': location})
+        all_hints = hints.find({'location': location})
     matching_hints = []
     for candidate in all_hints:
         if query.compare_answer(json.dumps(location), answer, candidate['answer']) == 'true':
@@ -139,7 +139,7 @@ def get_hint(mongodb, query, in_dict_json):
     out_hint_ids = [hint['_id'] for hint in out_hints]
     userspec = {
         'user': in_dict['user'],
-        'problem': location,
+        'location': location,
     }
     userdata = mongodb['userdata']
     user = userdata.find_one(userspec)
@@ -169,7 +169,7 @@ def hint_history(mongodb, query, in_dict_json):
     in_dict = json.loads(in_dict_json)
     spec = {
         'user': in_dict['user'],
-        'problem': in_dict['location']
+        'location': in_dict['location']
     }
     userdata = mongodb['userdata']
     matching_user = userdata.find_one(spec)
@@ -207,7 +207,7 @@ def vote(mongodb, query, in_dict_json):
     location = in_dict['location']
     spec = {
         'user': in_dict['user'],
-        'problem': location
+        'location': location
     }
     userdata = mongodb['userdata']
     matching_user = userdata.find_one(spec)
@@ -267,19 +267,22 @@ def submit_hint(mongodb, query, in_dict_json):
     userdata = mongodb['userdata']
     spec = {
         'user': in_dict['user'],
-        'problem': location,
+        'location': location,
     }
-
-    # Make sure the user is actually qualified to submit a hint.
-    matching_user = userdata.find_one(spec)
-    if matching_user['voted']:
-        return {'success': False,
-                'error': 'Already voted!'}
-
     problem_settings = mongodb['settings'].find_one({'location': location})
-    if problem_settings is None or problem_settings['display_only']:
-        return {'success': False,
-                'error': 'This problem does not accept hints!'}
+
+    # This is a temporary instructor bypass, until I figure out how authentication
+    # on edInsights works.
+    if in_dict['user'] != 'instructor':
+        # Make sure the user is actually qualified to submit a hint.
+        matching_user = userdata.find_one(spec)
+        if matching_user['voted']:
+            return {'success': False,
+                    'error': 'Already voted!'}
+
+        if problem_settings is None or problem_settings['display_only']:
+            return {'success': False,
+                    'error': 'This problem does not accept hints!'}
 
     if query.validate_answer(json.dumps(location), answer) != 'true':
         return {'success': False,
@@ -288,7 +291,7 @@ def submit_hint(mongodb, query, in_dict_json):
     # Now, add the hint to the database.
     hints = mongodb['hints']
     hints.insert({
-        'problem': location,
+        'location': location,
         'answer': answer,
         'hint': in_dict['hint'],
         'user': in_dict['user'],
@@ -331,10 +334,74 @@ def list_problems(mongodb, query, in_dict_json):
 @query()
 def dump_hints(mongodb, query, in_dict_json):
     """
-    Returns [answer, id, text, votes] for the problem
-    specified by 'location' in in_dict.
+    Returns hints for the problem specified by 'location' in in_dict.
+    Filters by the 'field' in in_dict, to display either problems
+    in the 'mod_queue' or approved 'hints'
     """
-    pass
+    in_dict = json.loads(in_dict_json)
+    location = in_dict['location']
+    if in_dict['field'] == 'mod_queue':
+        approved = False
+    else:
+        approved = True
+    dumped_hints = mongodb['hints'].find({
+        'location': location,
+        'settings.approved': approved,
+    })
+
+    # Make a list of [id, answer, hint text, votes] for each hint.
+    hint_list = []
+    for hint in dumped_hints:
+        hint_list.append([str(hint['_id']), hint['answer'], hint['hint'], hint['votes']])
+    return {'success': True,
+            'hints': hint_list,
+            'problem_name': query.get_name(json.dumps(location))}
+
+
+@query()
+def delete_hints(mongodb, query, in_dict_json):
+    """
+    Deletes hints, for instructors.
+    in_dict is expected to have key 'to_delete', corresponding to a list of
+    id's to delete.
+    """
+    to_delete = json.loads(in_dict_json)['to_delete']
+    hints = mongodb['hints']
+    for hint_id in to_delete:
+        hints.remove({'_id': ObjectId(hint_id)})
+    return {'success': True}
+
+
+@query()
+def change_votes(mongodb, query, in_dict_json):
+    """
+    Changes vote counts, for instructors.
+    in_dict is expected to have key 'updated_votes'.
+    in_dict['updated_votes'] is a list containing sublists of
+    [pk, new_votes], each of which representing a hint and the
+    new vote count.
+    """
+    updated_votes = json.loads(in_dict_json)['updated_votes']
+    hints = mongodb['hints']
+    for hint_id, new_votes in updated_votes:
+        hints.update({'_id': ObjectId(hint_id)},
+                     {'$set': {'votes': int(new_votes)}})
+    return {'success': True}
+
+
+@query()
+def approve_hints(mongodb, query, in_dict_json):
+    """
+    Approves hints, for instructors.
+    in_dict is expected to have key 'to_approve'
+    in_dict['to_approve'] is a list of pk's of hints to approve.
+    """
+    to_approve = json.loads(in_dict_json)['to_approve']
+    hints = mongodb['hints']
+    for hint_id in to_approve:
+        hints.update({'_id': ObjectId(hint_id)},
+                     {'$set': {'settings.approved': True}})
+    return {'success': True}
 
 
 @view()
